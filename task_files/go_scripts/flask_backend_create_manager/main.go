9ed826b6f7c8cb82341f10d97e312f33e924fdd5
windmillcode0 <shieldmousetower734@gmail.com>
2024-10-17 11:45:41 -0400

    [UPDATE] additional work

--- task_files/go_scripts/flask_backend_create_manager/main.go
+++ task_files/go_scripts/flask_backend_create_manager/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strings"
-
 	"main/shared"
+	"os"
+	"regexp"
+	"strings"
 
 	"github.com/windmillcode/go_cli_scripts/v5/utils"
 )
@@ -57,6 +57,7 @@ func main() {
 			ErrMsg: "You must provide the name of a manager",
 		},
 	)
+	// managerName :="feg"
 	managerNameString, err := utils.CreateStringObject(managerName, "")
 	snakeCaseManagerName := managerNameString.Snakecase(false, "_manager")
 	managersLocation = utils.JoinAndConvertPathToOSFormat(managersLocation, snakeCaseManagerName)
@@ -84,4 +85,85 @@ func main() {
 		utils.OverwriteFile(path, fileString)
 	}
 
+	updateConfigsFile(targetApp, managerNameString)
 }
+
+func updateConfigsFile(targetConfigs string, managerNameString utils.CreateStringObjectType) {
+	configsFile := utils.JoinAndConvertPathToOSFormat(targetConfigs, "configs.py")
+	contentBytes, err := os.ReadFile(configsFile)
+	if err != nil {
+			fmt.Printf("Error reading configs.py: %v\n", err)
+			return
+	}
+	contentStr := string(contentBytes)
+	contentStr = strings.ReplaceAll(contentStr, "\r", "")
+
+	// Determine the indentation (tabs or spaces)
+	indentation := "\t" // Default to tab
+	// Try to detect indentation from existing @property methods
+	propertyIndentRegex := regexp.MustCompile(`(?m)^(\s*)@property`)
+	indentMatch := propertyIndentRegex.FindStringSubmatch(contentStr)
+	if len(indentMatch) >= 2 {
+			indentation = indentMatch[1]
+	} else {
+			// If no @property found, detect indentation from def statements
+			defIndentRegex := regexp.MustCompile(`(?m)^(\s*)def `)
+			indentMatch = defIndentRegex.FindStringSubmatch(contentStr)
+			if len(indentMatch) >= 2 {
+					indentation = indentMatch[1]
+			} else {
+					// Default to 4 spaces if indentation cannot be detected
+					indentation = "    "
+			}
+	}
+	indentation = strings.ReplaceAll(indentation, "\n", "")
+
+
+	propertyRegex := regexp.MustCompile(`(?s)(@property\s+def\s+\w+\(self\):.*?return\s+self\._managers\["\w+"\])`)
+	matches := propertyRegex.FindAllStringIndex(contentStr, -1)
+
+	// Prepare the new code block with the correct indentation
+	newCodeBlock := fmt.Sprintf("\n\n%s@property\n%sdef %s_manager(self):\n%sif not self._managers.get(\"%s_manager\"):\n%sfrom managers.%s_manager.%s_manager import %sManager\n%sself._managers[\"%s_manager\"] = %sManager()\n%sreturn self._managers[\"%s_manager\"]\n",
+			indentation,
+			indentation,
+			managerNameString.Snakecase(false, ""),
+			indentation+indentation,
+			managerNameString.Snakecase(false, ""),
+			indentation+indentation+indentation,
+			managerNameString.Snakecase(false, ""),
+			managerNameString.Snakecase(false, ""),
+			managerNameString.Classify(false, ""),
+			indentation+indentation+indentation,
+			managerNameString.Snakecase(false, ""),
+			managerNameString.Classify(false, ""),
+			indentation+indentation,
+			managerNameString.Snakecase(false, ""))
+
+	if len(matches) == 0 {
+			// No @property methods found, add to class
+			classRegex := regexp.MustCompile(`(?s)(class\s+\w+\(.*?\):)`)
+			classMatch := classRegex.FindStringIndex(contentStr)
+			if len(classMatch) > 0 {
+					insertPos := classMatch[1]
+					updatedContent := contentStr[:insertPos] + "\n" + indentation + newCodeBlock + contentStr[insertPos:]
+					err = os.WriteFile(configsFile, []byte(updatedContent), 0644)
+					if err != nil {
+							fmt.Printf("Error writing to configs.py: %v\n", err)
+					}
+			} else {
+					fmt.Println("No class definition found in configs.py")
+					return
+			}
+	} else {
+			// Insert after the last @property method
+			lastMatch := matches[len(matches)-1]
+			insertPos := lastMatch[1]
+			updatedContent := contentStr[:insertPos] + newCodeBlock + contentStr[insertPos:]
+			err = os.WriteFile(configsFile, []byte(updatedContent), 0644)
+			if err != nil {
+					fmt.Printf("Error writing to configs.py: %v\n", err)
+			}
+	}
+}
+
+

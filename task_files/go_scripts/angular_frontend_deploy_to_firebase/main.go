4597f9ca45f730cdd647ff9184e899f233bc9b6c
windmillcode0 <shieldmousetower734@gmail.com>
2024-11-29 02:00:07 -0500

    [UPDATE] additional work

--- task_files/go_scripts/angular_frontend_deploy_to_firebase/main.go
+++ task_files/go_scripts/angular_frontend_deploy_to_firebase/main.go
@@ -5,8 +5,14 @@ import (
 	"os"
 	"sync"
 	"github.com/windmillcode/go_cli_scripts/v5/utils"
-)
+	"strings"
 
+	"main/shared"
+)
+var(
+	taskSettings = utils.AngularDeployToFirebaseStruct {}
+	myGlobal = map[string]string{}
+)
 func main() {
 	// COMMAND LINE OPTIONS
 	//
@@ -15,40 +21,85 @@ func main() {
 	// Change directory to fbDest and deploy using Firebase
 	// Remove the directories
 	// Change back to the workspace root
+	shared.CDToWorkspaceRoot()
+
+	workspaceRoot, err := os.Getwd()
+	settings, err := utils.GetSettingsJSON(workspaceRoot)
+	if err != nil {
+		return
+	}
 	utils.SetGlobalVars(
 		utils.SetGlobalVarsOptions{
-			NonInteractive: utils.ProcessIfDefaultIsPresentStruct{
-				Global: true,
-			},
+			NonInteractive :settings.ExtensionPack.ProcessIfDefaultIsPresent,
 		},
 	)
+	taskSettings = settings.ExtensionPack.AngularDeployToFirebase
 
 	os.Setenv("NO_UPDATE_NOTIFIER", "true")
 
 	envsInfo := utils.TakeVariableArgs(
 		utils.TakeVariableArgsStruct{
 			Prompt:  "Provide the environments that should be part of this deployment",
-			Default: "prod preview",
+			Default: strings.Join(taskSettings.Environments, " "),
 		},
 	)
 	cliInfo := utils.ShowMenuModel{
 		Prompt: "run lint",
 		Choices:[]string{"TRUE", "FALSE"},
-		Default: "TRUE",
+		Default: func() string {
+			if taskSettings.RunLint {
+				return "TRUE"
+			}
+			return "FALSE"
+		}(),
 	}
 	runLint := utils.ShowMenu(cliInfo,nil)
 	cliInfo = utils.ShowMenuModel{
 		Prompt: "run ssg script",
 		Choices:[]string{"TRUE","FALSE"},
-		Default: "TRUE",
+		Default: func() string {
+			if taskSettings.RunSSGScript {
+				return "TRUE"
+			}
+			return "FALSE"
+		}(),
 	}
 	runSSGScript := utils.ShowMenu(cliInfo,nil)
 	cliInfo = utils.ShowMenuModel{
 		Prompt: "remove build directories",
 		Choices:[]string{"TRUE","FALSE"},
-		Default: "TRUE",
+		Default: func() string {
+			if taskSettings.RemoveBuildDirectories {
+				return "TRUE"
+			}
+			return "FALSE"
+		}(),
 	}
 	removeBuildDir := utils.ShowMenu(cliInfo,nil)
+	cliInfo = utils.ShowMenuModel{
+		Prompt: "deploy to sentry",
+		Choices:[]string{"TRUE","FALSE"},
+		Default: func() string {
+			if taskSettings.DeployToSentry {
+				return "TRUE"
+			}
+			return "FALSE"
+		}(),
+	}
+	myGlobal["deployToSentry"]  = utils.ShowMenu(cliInfo,nil)
+	cliInfo = utils.ShowMenuModel{
+		Prompt: "deploy to firebase",
+		Choices:[]string{"TRUE","FALSE"},
+		Default: func() string {
+			if taskSettings.DeployToFirebase {
+				return "TRUE"
+			}
+			return "FALSE"
+		}(),
+	}
+	myGlobal["deployToFirebase"]  = utils.ShowMenu(cliInfo,nil)
+
+
 	envs := envsInfo.InputArray
 	var wg sync.WaitGroup
 	for _, v := range envs {
@@ -67,17 +118,16 @@ func buildAndDeploy(env,runLint,runSSGScript,removeBuildDir string) {
 
 	// CLI ARGS
 	targetEnviromnent, _ := utils.CreateStringObject(env, "")
-	os.Setenv("SENTRY_ORG", "windmilcode")
-	os.Setenv("SENTRY_PROJECT", "eneobia")
-	os.Setenv("SENTRY_AUTH_TOKEN", "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")
-	firebaseProjectId := "eneobia"
+	os.Setenv("SENTRY_ORG", taskSettings.SentryOrg)
+	os.Setenv("SENTRY_PROJECT", taskSettings.SentryProject)
+	os.Setenv("SENTRY_AUTH_TOKEN", taskSettings.SentryAuthToken)
+	firebaseProjectId := taskSettings.FirebaseProjectId
 	//
 	if targetEnviromnent.Orig == "preview" {
 		firebaseProjectId += "-preview"
 	}
 
-	currentFolder, _ := os.Getwd()
-	workSpaceRoot := utils.JoinAndConvertPathToOSFormat(currentFolder, "..", "..")
+	workSpaceRoot, _ := os.Getwd()
 
 	appSource := utils.JoinAndConvertPathToOSFormat(workSpaceRoot, "apps", "frontend", "AngularApp")
 	appDistSource := utils.JoinAndConvertPathToOSFormat(workSpaceRoot, "apps", "frontend", "AngularApp", "dist", fmt.Sprintf("angular-app-%s", targetEnviromnent.Orig), "browser")
@@ -88,7 +138,8 @@ func buildAndDeploy(env,runLint,runSSGScript,removeBuildDir string) {
 			Command:              "npm",
 			Args:                 []string{"run", "lint"},
 			TargetDir:            appSource,
-			PrintOutputOnly:      true ,
+			GetOutput:       false,
+			PrintOutputOnly: true,
 		}
 		utils.RunCommandWithOptions(lintOptions)
 	}
@@ -98,6 +149,8 @@ func buildAndDeploy(env,runLint,runSSGScript,removeBuildDir string) {
 		Command:           "npm",
 		Args:              []string{"run", fmt.Sprintf("build:%s", targetEnviromnent.Orig)},
 		TargetDir:         appSource,
+		GetOutput:       false,
+		PrintOutputOnly: true,
 	}
 	utils.RunCommandWithOptions(buildOptions)
 
@@ -111,37 +164,45 @@ func buildAndDeploy(env,runLint,runSSGScript,removeBuildDir string) {
 		utils.RunCommandWithOptions(ssgOptions)
 	}
 
-
-		if err := os.RemoveAll(angularBuildDestination); err != nil {
-			fmt.Println("Error removing old build destination:", err)
-			return
-		}
-
+	if err := os.RemoveAll(angularBuildDestination); err != nil {
+		fmt.Println("Error removing old build destination:", err)
+		return
+	}
 
 	if err := utils.CopyDir(appDistSource, angularBuildDestination); err != nil {
 		fmt.Println("Error copying from the dist source in the AngularApp to the destination in the FirebaseApp", err)
 		return
 	}
 
-	sourceMapInjectOptions := utils.CommandOptions{
-		Command: "sentry-cli",
-		Args:    []string{"sourcemaps", "inject", appDistSource},
-	}
-	utils.RunCommandWithOptions(sourceMapInjectOptions)
+	if myGlobal["deployToSentry"] == "TRUE" {
+		sourceMapInjectOptions := utils.CommandOptions{
+			Command: "sentry-cli",
+			Args:    []string{"sourcemaps", "inject", appDistSource},
+			GetOutput:       false,
+			PrintOutputOnly: true,
+		}
+		utils.RunCommandWithOptions(sourceMapInjectOptions)
 
-	sourceMapUploadOptions := utils.CommandOptions{
-		Command: "sentry-cli",
-		Args:    []string{"sourcemaps", "upload", appDistSource, "--", "--env", fmt.Sprintf("Angular_%s", targetEnviromnent.Uppercase(false, ""))},
+		sourceMapUploadOptions := utils.CommandOptions{
+			Command: "sentry-cli",
+			Args:    []string{"sourcemaps", "upload", appDistSource, "--", "--env", fmt.Sprintf("Angular_%s", targetEnviromnent.Uppercase(false, ""))},
+			GetOutput:       false,
+			PrintOutputOnly: true,
+		}
+		utils.RunCommandWithOptions(sourceMapUploadOptions)
 	}
-	utils.RunCommandWithOptions(sourceMapUploadOptions)
 
-	deployOptions := utils.CommandOptions{
-		Command:         "npx",
-		Args:            []string{"firebase", "deploy", "--only", "hosting", "--config", fmt.Sprintf("angular.firebase.%s.json", targetEnviromnent.Orig), "--project", firebaseProjectId},
-		TargetDir:       fbDest,
-		PrintOutputOnly: true,
+
+	if myGlobal["deployToFirebase"] == "TRUE" {
+		deployOptions := utils.CommandOptions{
+			Command:         "npx",
+			Args:            []string{"firebase", "deploy", "--only", "hosting", "--config", fmt.Sprintf("angular.firebase.%s.json", targetEnviromnent.Orig), "--project", firebaseProjectId},
+			TargetDir:       fbDest,
+			PrintOutputOnly: true,
+		}
+		utils.RunCommandWithOptions(deployOptions)
 	}
-	utils.RunCommandWithOptions(deployOptions)
+
 
 	if err := os.RemoveAll(angularBuildDestination); err != nil {
 		fmt.Println("Error removing firebase build destination:", err)

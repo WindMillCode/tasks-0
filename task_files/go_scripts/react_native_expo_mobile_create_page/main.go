b869465ea4834cae434397b4bff485ef356e9de8
windmillcode0 <shieldmousetower734@gmail.com>
2024-12-07 13:23:37 -0500

    [UPDATE] additional work

--- task_files/go_scripts/react_native_expo_mobile_create_page/main.go
+++ task_files/go_scripts/react_native_expo_mobile_create_page/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"main/shared"
 	"os"
+	"strings"
+
 	"github.com/windmillcode/go_cli_scripts/v6/utils"
 )
 
 func main() {
 
+	scriptLocation, err := os.Getwd()
+	if err != nil {
+		return
+	}
+	templateLocation := utils.JoinAndConvertPathToOSFormat(scriptLocation, "template")
 	shared.CDToWorkspaceRoot()
 	workspaceRoot, err := os.Getwd()
 	settings, err := utils.GetSettingsJSON(workspaceRoot)
@@ -19,11 +27,36 @@ func main() {
 			NonInteractive: settings.ExtensionPack.ProcessIfDefaultIsPresent,
 		},
 	)
-	utils.CDToSeleniumApp()
 
-	opts := utils.CommandOptions{
-		Command: "",
-		Args:    []string{},
+	pageName := utils.GetInputFromStdin(
+		utils.GetInputFromStdinStruct{
+			Prompt: []string{"The name you would like to give to the page"},
+			ErrMsg: "You must provide a value",
+		},
+	)
+	pageNameObject,_ := utils.CreateStringObject(pageName,"")
+
+	utils.CDToReactNativeExpoApp()
+	reactNativeExpoApp, err := os.Getwd()
+	if err != nil {
+		return
 	}
-	utils.RunCommandWithOptions(opts)
+	providerLocation := utils.JoinAndConvertPathToOSFormat(reactNativeExpoApp, "app", fmt.Sprintf("(%s)",pageNameObject.Snakecase(false,"")) )
+	utils.CopyDir(templateLocation, providerLocation)
+
+	utils.TraverseDirectory(utils.TraverseDirectoryParams{
+		RootDir: providerLocation,
+		Predicate: func(srcPath string, info os.FileInfo) {
+			fileString, err := utils.ReadFile(srcPath)
+			if err != nil {
+				return
+			}
+			fileString = strings.ReplaceAll(fileString, "WMLTemplate", pageNameObject.Orig)
+			utils.OverwriteFile(srcPath, fileString)
+		},
+		Filter: func(srcPath string, info os.FileInfo) bool {
+			return !info.IsDir()
+		},
+	})
+
 }
